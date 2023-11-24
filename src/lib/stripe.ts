a3a3aa1c80238b233c66b68c62d7438aa012ef67
Matheus Magno <matheusmagnon@gmail.com>
2023-11-24 20:17:52 -0300

    edit: stripe API file

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,9 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(<any>process.env.STRIPE_SECRET_KEY, {
+export const stripe = new Stripe(
+  <string>process.env.STRIPE_SECRET_KEY,
+  //"sk_test_51MaNxXBG08EyYbkiqN0CHZN6hcOgkKwXfM0ESew4XjfB0gcLoaaID8QZOvC4S9WvFhwwvjwAU3sx4C5ZsuIJad0b00UN0BVARS",
+  {
   apiVersion: "2022-11-15",
   appInfo: { name: "Next Shop" },
 });
