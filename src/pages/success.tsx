b3c3d4a35b35ad9b55b6ba026cb23787b5aca4c5
Matheus Magno <matheusmagnon@gmail.com>
2023-12-14 21:48:42 -0300

    style: success page

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,24 @@
+
+import Link from "next/link"
+import {ImageContainer,SuccessContainer} from "../styles/pages/success"
+import React from "react"
+
 export default function Success() {
-  return <h1>Success</h1>;
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+      <p>
+        Uhuul <strong>Matheus Magno</strong>,
+        sua <strong>Camiseta xxx</strong> já está a caminho da sua casa.
+      </p>
+
+      <Link href='/'>
+        Voltar ao catálogo
+      </Link>
+    </SuccessContainer>
+  )
 }
